5aed551d77b3f451216948f2c6d9635d267abb4a
Erik-luna <erikjesusaavedraluna@gmail.com>
2026-09-16 04:40:44 -0400

    Agregar trustProxies para solucionar contenido mixto en Swagger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request; // <-- Asegúrate de incluir esta línea arriba
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Confiar en todos los proxies de la plataforma en la nube (Northflank)
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth' => \App\Http\Middleware\Autenticar::class,
             'rol' => \App\Http\Middleware\VerificarRol::class,
